fcdebf11266f0881539a5eb7fc6e6641295c9988
silujnr <silujnr@gmail.com>
2022-05-08 22:22:43 +0530

    Update login.js

--- ato-dashboard-ui/src/pages/login.js
+++ ato-dashboard-ui/src/pages/login.js
@@ -1,71 +1,54 @@
-import React, {Component} from 'react';
-import axios from 'axios';
- import {Link, Redirect} from 'react-router-dom';
+ import React, {Component} from 'react';
+  import {Link} from 'react-router-dom';
  import TitleComponent from "./title";
-export default class Login extends Component {
 
-    state = {
-        email: '',
-        password: '',
-        redirect: false,
-        authError: false,
-        isLoading: false,
-        location: {},
-    };
 
-    handleEmailChange = event => {
-        this.setState({email: event.target.value});
-    };
-    handlePwdChange = event => {
-        this.setState({password: event.target.value});
-    };
-
-    handleSubmit = event => {
-        event.preventDefault();
-        this.setState({isLoading: true});
-        const url = 'https://gowtham-rest-api-crud.herokuapp.com/login';
-        const email = this.state.email;
-        const password = this.state.password;
-        let bodyFormData = new FormData();
-        bodyFormData.set('email', email);
-        bodyFormData.set('password', password);
-        axios.post(url, bodyFormData)
-            .then(result => {
-                if (result.data.status) {
-                    localStorage.setItem('token', result.data.token);
-                    this.setState({redirect: true, isLoading: false});
-                    localStorage.setItem('isLoggedIn', true);
-                }
-            })
-            .catch(error => {
-                console.log(error);
-                this.setState({authError: true, isLoading: false});
-            });
-    };
-
-    // componentDidMount() {
-    //     const url = 'https://freegeoip.app/json/';
-    //     axios.get(url)
-    //         .then(response => {
-    //             const location = response.data;
-    //             this.setState({ location });
-    //         }) 
-    //         .catch(error => {
-    //             this.setState({ toDashboard: true });
-    //             console.log(error);
-    //         });
-    // }
-
-    renderRedirect = () => {
-        if (this.state.redirect) {
-            return <Redirect to='/dashboard'/>
+class Login extends Component {
+    constructor() {
+        super();
+ 
+        this.state = {
+            Email: '',
+            Password: ''
         }
-    };
-
+ 
+        this.Password = this.Password.bind(this);
+        this.Email = this.Email.bind(this);
+        this.login = this.login.bind(this);
+    }
+ 
+    Email(event) {
+        this.setState({ Email: event.target.value })
+    }
+    Password(event) {
+        this.setState({ Password: event.target.value })
+    }
+    login(event) {
+        debugger;
+        fetch('http://3.8.16.176:8080/ato-endpoints/login', {
+            method: 'post',
+            headers: {
+                'Accept': 'application/json',
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({
+                Email: this.state.Email,
+                Password: this.state.Password
+            })
+        }).then((Response) => Response.json())
+            .then((result) => {
+                console.log(result);
+                if (result.Status === 'Invalid')
+                    alert('Invalid User');
+                else
+                    this.props.history.push("/dashboard");
+            })
+    }
+ 
     render() {
-        const isLoading = this.state.isLoading;
         return (
-            <div className="login">
+
+<div className="login">
                 <TitleComponent title="ATO Login "></TitleComponent>
            
               
@@ -77,13 +60,14 @@ export default class Login extends Component {
                                 <div><label className="label">Login</label></div>
                                 <div><Link to={'register'}>New user - Register here</Link></div>
                             </div>
-                            <form onSubmit={this.handleSubmit}>
+                            <form>
                                 <div className='container'>
                                 <div className="row mb-0">
                                     <div className="input-icon">
                                         <i className="fa fa-user" aria-hidden="true"></i>
-                                       <input className={"border-radius-bottom0 margin-bottom-1px " + (this.state.authError ? 'is-invalid' : '')} id="inputEmail" placeholder="Email address" type="text" name="email" onChange={this.handleEmailChange} autoFocus required/>
-                                        {/* <label htmlFor="inputEmail">Email address</label> */}
+                                        <input className="border-radius-bottom0 margin-bottom-1px" type="text" 
+                                                 onChange={this.Email} 
+                                                 placeholder="Enter Email" />
                                     <div className="invalid-feedback">
                                         Please provide a valid Email.
                                     </div>
@@ -92,8 +76,9 @@ export default class Login extends Component {
                                 <div className="row mb22">
                                     <div className="input-icon">
                                         <i className="fa fa-unlock-alt" aria-hidden="true"></i>
-                                       <input type="password" className={"border-radius-top0 " + (this.state.authError ? 'is-invalid' : '')} id="inputPassword" placeholder="******" name="password" onChange={this.handlePwdChange} required/>
-                                       {/* <label htmlFor="inputPassword">Password</label> */}
+                                       <input className="border-radius-top0" type="text" 
+                                                 onChange={this.Password} 
+                                                 placeholder="*******" />
                                     <div className="invalid-feedback">
                                         Please provide a valid Password.
                                     </div>
@@ -101,14 +86,13 @@ export default class Login extends Component {
                                     {/* <span className="field-validation-error"> Please add error message here</span>  */}
                                 </div>
                                 <div className="d-flex valign-top row">
-                                <button className="btn btn-primary btn-block submit" type="submit" disabled={this.state.isLoading ? true : false}>Login &nbsp;&nbsp;&nbsp;
-                                    {isLoading ? (
+                                <button className="btn btn-primary btn-block submit" type="submit" onClick={this.login} >Login
+                                    {/* {isLoading ? (
                                         <span className="spinner-border spinner-border-sm" role="status" aria-hidden="true"></span>
                                     ) : (
                                         <span></span>
-                                    )}
+                                    )} */}
                                 </button>
-                                    {/* <input type="submit" id="submit-btn" value="Login" class="submit" /> */}
                                     <div><a href="forgot-password.html">Forgot Password?</a></div>
                                 </div>
                                 </div>
@@ -119,11 +103,14 @@ export default class Login extends Component {
                 <p className="login-text-center">Copyright © 2022 ATO. All Rights Reserved. </p>
                 </section>  
             
-            {this.renderRedirect()}
+            {/* {this.renderRedirect()} */}
             </div>
+
+
+             
+            
         );
     }
 }
-
-
-
+ 
+export default Login;
